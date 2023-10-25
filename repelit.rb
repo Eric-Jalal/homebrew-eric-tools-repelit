db0f66b71cc4b9bb9f9e020aa0da925ff305fcf1
Eric Jalal <ericjalal.fi@gmail.com>
2023-10-25 22:12:26 +0300

    Update repelit.rb

--- repelit.rb
+++ repelit.rb
@@ -2,7 +2,6 @@ class Repelit < Formula
   desc "A command-line tool to find and replace text in files"
   homepage "https://github.com/Eric-Jalal/homebrew-eric-tools-repelit"
   url "https://github.com/Eric-Jalal/eric-tools-repelit/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0ffde105f0b7e4f8b13d26db50a44c0dc33852313004a7f9c6c0eef48d18e7d4"
   version "1.0.0"
   license "MIT"
 
