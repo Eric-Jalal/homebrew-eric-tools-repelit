47d4d11152420c4c8a05e12ef7773cc873dd9fe9
Eric Jalal <ericjalal.fi@gmail.com>
2024-01-04 02:23:12 +0200

    Update inplace.rb

--- inplace.rb
+++ inplace.rb
@@ -1,7 +1,7 @@
 class Inplace < Formula
   desc "A command-line tool to find and replace text in files"
   homepage "https://github.com/Eric-Jalal/eric-tools-repelit"
-  url "https://github.com/Eric-Jalal/inplace/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/Eric-Jalal/inplace/releases/download/v1.0.0/inplace-v1.0.0.tar.gz"
   name "inplace"
   sha256 "7a822ac0ff2abd28144327d077f736a82e6c2165d4bbee54d0f0773f4445017b"
   version "1.0.0"
